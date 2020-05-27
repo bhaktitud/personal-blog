c867a2834cf6a206006f6a6697f9e65ccfd36a9a
bhaktitud <myforework@live.com>
2020-05-27 20:52:05 +0700

    home changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,13 +7,13 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = "BhaktixDev Personal Blog"
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
           title="Home"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+          keywords={[`blog`, `gatsby`, `javascript`, `react`, `tutorial`]}
         />
         <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
         <h1>
@@ -22,10 +22,9 @@ class IndexPage extends React.Component {
             👋
           </span>
         </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
+        <p>Hi! I'm Bhakti</p>
         <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
+          I am a full stack developer, currently love to write and share my knowledge to the open world!
         </p>
         <p>Now go build something great!</p>
         <Link to="/blog/">

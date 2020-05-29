fa0ad91b9d99a47a646f801085deaef4bb13215a
bhaktitud <myforework@live.com>
2020-05-29 14:46:08 +0700

    contact submission

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,13 +9,16 @@ class About extends React.Component {
         const siteTitle = data.site.siteMetadata.title
         return(
             <Layout location={this.props.location} title={siteTitle}>
-                {/* <div
+                <div
                     style={{ 
                         margin: "20px 0 40px",
+                        display: 'flex',
+                        flexDirection: 'column'
                     }}
-                > */}
-                        {/* <form
-                            name="contact"
+                >
+                        <h2>Reach out!</h2>
+                        <form
+                            name="contact-user"
                             method="POST" 
                             data-netlify="true"
                             style={{
@@ -32,48 +35,12 @@ class About extends React.Component {
                             <label for="email">Email</label>
                             <input type="email" id="email" name="email" placeholder="Your email address.." />
 
-                            <label for="country">Country</label>
-                            <select id="country" name="country">
-                            <option value="australia">Australia</option>
-                            <option value="canada">Canada</option>
-                            <option value="usa">USA</option>
-                            <option value="usa">Indonesia</option>
-                            </select>
-
                             <label for="subject">Subject</label>
                             <textarea id="subject" name="subject" placeholder="Write something.." style={{height:"200px"}}></textarea>
-
+                            
                             <button className="btn btn-primary" type="submit">Submit</button>
-                        </form> */}
-                        <form 
-                            name="contact-user" 
-                            method="POST" 
-                            data-netlify="true" 
-                            style={{
-                                display: 'flex',
-                                flexDirection: 'column' 
-                            }}
-                        >
-                            <p>
-                                <label>Your Name: <input type="text" name="name" /></label>   
-                            </p>
-                            <p>
-                                <label>Your Email: <input type="email" name="email" /></label>
-                            </p>
-                            <p>
-                                <label>Your Role: <select name="role[]" multiple>
-                                <option value="leader">Leader</option>
-                                <option value="follower">Follower</option>
-                                </select></label>
-                            </p>
-                            <p>
-                                <label>Message: <textarea name="message"></textarea></label>
-                            </p>
-                            <p>
-                                <button type="submit" className="btn btn-primary">Send</button>
-                            </p>
                         </form>
-                {/* </div> */}
+                </div>
             </Layout>
         )
     }

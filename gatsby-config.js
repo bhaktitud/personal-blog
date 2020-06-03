a9a30d70582588f04dce918bda64888ac2dda818
bhaktitud <myforework@live.com>
2020-06-03 14:18:32 +0700

    trackingid analytics added

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,7 +79,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-44860260-1`,
       },
     },
     {
